dcc04fb9ee2d252ba23094f3ba0c5b4b5609d1ff
IkadaiYuya <yuuyayou@gmail.com>
2018-12-19 21:38:22 +0900

    2018/12/19
    敵の行動処理更新
    敵の攻撃管理追加

---
+++ Assets/Scripts/MainGame/EnemyAttackManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackManager : MonoBehaviour {
+
+    //
+    private List<GameObject> enemyGarage = new List<GameObject>();
+    //
+    [SerializeField] private float attackIntervalTime = 10.0f;
+    //
+    private float attackTimer;
+
+	// Use this for initialization
+	void Start () {
+        attackTimer = attackIntervalTime;
+        EnemyGarageControll();
+        Debug.Log("EnemyGarageNum:" + enemyGarage.Count);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        attackTimer -= Time.deltaTime;
+		if(attackTimer < 0)
+        {
+            Debug.Log("EnemyAttack");
+            RandomAttack();
+            attackTimer = attackIntervalTime;
+        }
+	}
+
+    //
+    private void RandomAttack()
+    {
+        int attackEnemyNum = Random.Range(0, enemyGarage.Count-1);
+        EnemyController eCon = enemyGarage[attackEnemyNum].GetComponent<EnemyController>();
+        eCon.CurrentEnemyState = EnemyController.EnemyState.Charge;
+    }
+
+    //public method
+    //
+    public void RemenberEnemy(GameObject deadEne)
+    {
+        for(int i = enemyGarage.Count -1; i >= 0;--i)
+        {
+            if(enemyGarage[i] == deadEne)
+            {
+                enemyGarage.RemoveAt(i);
+            }
+        }
+    }
+
+    //
+    public void EnemyGarageControll()
+    {
+        GameObject[] tmpEne = GameObject.FindGameObjectsWithTag("Enemy");
+        for(int i = 0; i < tmpEne.Length;++i)
+        {
+            enemyGarage.Add(tmpEne[i]);
+        }
+        //foreach(GameObject obj in UnityEngine.Object.FindObjectsOfType(typeof(GameObject)))
+        //{
+        //    if(obj.tag == "Enemy")
+        //    {
+        //        for(int i = enemyGarage.Count - 1; i >= 0;--i)
+        //        {
+        //            if(enemyGarage[i] == obj)
+        //            {
+        //                Debug.Log("Break");
+        //                break;
+        //            }
+        //            if(i == 0)
+        //            {
+        //                enemyGarage.Add(obj);
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}
--- Assets/Scripts/MainGame/EnemyController.cs
+++ Assets/Scripts/MainGame/EnemyController.cs
@@ -4,34 +4,72 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    //敵の状態
     public enum EnemyState
     {
        Non = -1,
        Stand,
        Walk,
+       Chase,
+       Charge,
        ShortAttack,
        LongAttack,
-
     }
-    // this Charactor Move Direction
+    //
+    private EnemyState enemyState = EnemyState.Stand;
+    //移動量
     private Vector3 moveDirection;
-    // this Charactor Move Speed
-    [SerializeField] private float moveSpeed = 10.0f;
-    // this Charactor Rotetion Speed
+    //通常移動速度
+    [SerializeField] private float moveSpeed = 5.0f;
+    //
+    [SerializeField] private float moveRotPaSec = 10.0f;
+    //
+    [SerializeField] private float chaseSpeed = 1.0f;
+    //回転速度
     [SerializeField] private float rotSpeed = 360.0f;
-    // Distance to Target
+    //
+    [SerializeField] private float chaceRange = 3.0f;
+    //
+    private Vector3 rotDistance;
+    //
+    private float motionTimer;
+    //
+    [SerializeField] private float walkChengeTime = 2.0f;
+    //
+    [SerializeField] private float chargeTime = 1.0f;
+    //
+    [SerializeField] private float shortAttackTime = 1.0f;
+    //
+    [SerializeField] private float longAttackTime = 1.0f;
+    //
+    [SerializeField] private float backTime = 0.2f;
+    //
+    [SerializeField] private float punchRange = 2.0f;
+    //標的との距離
     private float targetRenge;
-    // this Charactor Gravity
+    //
+    private float targetRad;
+    //重力
     [SerializeField] private float gravity = -9.81f;
-    // this Charactor Controller
+    //
+    private Transform transform;
+    //キャラクターコントローラー
     private CharacterController cCon;
-    // this Animator
+    //アニメーター
     private Animator animator;
-    // Target Charactor Transform
+    //標的のトランスフォーム
     [SerializeField] private Transform targetT;
+
+    //
+    public EnemyState CurrentEnemyState
+    {
+        get {return enemyState; }
+        set { enemyState = value; }
+    }
     // Use this for initialization
     void Start()
     {
+        transform = GetComponent<Transform>();
         cCon = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
     }
@@ -39,7 +77,173 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (targetT == null)
+        {
+            return;
+        }
+        DecTimer();
+        RotationChara();
+        CheckTargetRenge();
+        Think();
+        if (Input.GetButtonDown("A"))
+        {
+            CurrentEnemyState = EnemyState.Charge;
+            motionTimer = chargeTime;
+        }
+        Move();
+        Animation();
     }
 
+    //
+    private void DecTimer()
+    {
+        motionTimer -= Time.deltaTime;
+    }
 
+    //
+    private void RotationChara()
+    {
+        if(CurrentEnemyState == EnemyState.ShortAttack)
+        {
+            return;
+        }
+        rotDistance = (targetT.position - transform.position).normalized;
+        rotDistance.y = 0;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(rotDistance), rotSpeed * Time.deltaTime);
+    }
+
+    //
+    private void CheckTargetRenge()
+    {
+        targetRenge = Vector3.Distance(targetT.position,transform.position);
+        Debug.Log("TargetRenge:" + targetRenge);
+    }
+
+    //
+    private void Think()
+    {
+        if(motionTimer > 0)
+        {
+            return;
+        }
+        switch (CurrentEnemyState)
+        {
+            case EnemyState.Stand:
+                if(targetT != null)
+                {
+                    if (targetRenge >= chaceRange)
+                    {
+                        CurrentEnemyState = EnemyState.Chase;
+                    }
+                    else
+                    {
+                        CurrentEnemyState = EnemyState.Walk;
+                        motionTimer = walkChengeTime;
+                    }
+                }
+                break;
+            case EnemyState.Walk:
+                if(targetT == null)
+                {
+                    CurrentEnemyState = EnemyState.Stand;
+                    return;
+                }
+                if (targetRenge >= chaceRange)
+                {
+                    CurrentEnemyState = EnemyState.Chase;
+                }
+
+                break;
+            case EnemyState.Chase:
+                if(targetT == null)
+                {
+                    CurrentEnemyState = EnemyState.Stand;
+                }
+                else if(targetRenge < chaceRange)
+                {
+                    CurrentEnemyState = EnemyState.Walk;
+                    motionTimer = walkChengeTime;
+                }
+                break;
+            case EnemyState.Charge:
+                if(targetRenge <= punchRange)
+                {
+                    CurrentEnemyState = EnemyState.ShortAttack;
+                    motionTimer = shortAttackTime;
+                }
+                else
+                {
+                    CurrentEnemyState = EnemyState.LongAttack;
+                    motionTimer = longAttackTime;
+                }
+                break;
+            case EnemyState.ShortAttack:
+                CurrentEnemyState = EnemyState.Stand;
+                break;
+            case EnemyState.LongAttack:
+                CurrentEnemyState = EnemyState.Stand;
+                break;
+        }
+    }
+
+    //
+    private void Move()
+    {
+        Debug.Log("EnemyState:" + CurrentEnemyState);
+        switch (CurrentEnemyState)
+        {
+            case EnemyState.Stand:
+                break;
+            case EnemyState.Walk:
+                CheckNextPos();
+                cCon.Move(moveDirection);
+                break;
+            case EnemyState.Chase:
+                moveDirection = targetT.position - transform.position;
+                moveDirection *= chaseSpeed * Time.deltaTime;
+                moveDirection.y = 0;
+                cCon.Move(moveDirection);
+                break;
+            case EnemyState.Charge:
+                break;
+            case EnemyState.ShortAttack:
+                break;
+            case EnemyState.LongAttack:
+                break;
+        }
+    }
+
+    //
+    private void Animation()
+    {
+        switch (CurrentEnemyState)
+        {
+            case EnemyState.Stand:
+                break;
+            case EnemyState.Walk:
+                break;
+            case EnemyState.Charge:
+                break;
+            case EnemyState.ShortAttack:
+                break;
+            case EnemyState.LongAttack:
+                break;
+        }
+    }
+
+    //
+    private void CheckNextPos()
+    {
+        int dis = 1;
+        if(motionTimer <= 0)
+        {
+            dis = Random.Range(0, 3) - 1;
+            dis = dis == 0 ? 1 : dis;
+        }
+        targetRad = Mathf.Atan2(transform.position.z - targetT.position.z, transform.position.x - targetT.position.x);
+        targetRad += moveRotPaSec * Mathf.Deg2Rad;
+        Vector3 worldVec = targetT.position + new Vector3(Mathf.Cos(targetRad), 0, Mathf.Sin(targetRad)) * targetRenge;
+        moveDirection = (worldVec - transform.position) * moveSpeed * Time.deltaTime;
+        moveDirection.y = 0;
+    }
 }
